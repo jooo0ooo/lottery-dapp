0b7730e3b2d9b33b1cf70325ec76d0863c188e2d
joo <pingrae@me.com>
2021-11-22 18:20:14 +0900

    add bet function && its test code

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,13 +2,76 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
+    struct BetInfo {
+        uint256 answerBlockNumber;
+        address payable bettor;
+        bytes1 challenges;
+    }
+
+    uint private _tail;
+    uint private _head;
+    mapping (uint256 => BetInfo) private _bets;
     address public owner;
+
+    uint256 constant internal BLOCK_LIMIT = 256;
+    uint256 constant internal BET_BLOCK_INTERVAL = 3;
+    uint256 constant internal BET_AMOUNT = 5 * 10 ** 15;
+
+    uint256 private _pot;
+
+    event BET(uint256 index, address bettor, uint256 amount, bytes1 challenges, uint256 answerBlockNumber);
 
     constructor() {
         owner = msg.sender;
     }
 
-    function getSomeValue() public pure returns (uint256 value) {
-        return 5;
+    function getPot() public view returns (uint256 pot) {
+        return _pot;
+    }
+
+    /**
+     * @dev 베팅을 한다. 유저는 0.005 ETH를 보내야 하고, 베팅용 1 byte 글자를 보낸다.
+     * 큐에 저장된 베팅 정보는 이후 distribute 함수에서 해결된다.
+     * @param challenges 유저가 베팅하는 글자
+     * @return result 함수가 잘 수행되었는지 확인 해주는 bool 값 
+     */
+    function bet(bytes1 challenges) public payable returns (bool result) {
+        // Check the proper ether is sent
+        require(msg.value == BET_AMOUNT, "Not enough ETH");
+
+        // Push bet to the queue
+        require(pushBet(challenges), "Fail to add a new Bet Info");
+
+        // Emit event
+        emit BET(_tail - 1, msg.sender, msg.value, challenges, block.number + BET_BLOCK_INTERVAL);
+
+        return true;
+    }
+        //save the bet to the queue
+
+    // Distribute
+        // check the answer
+
+    function getBetInfo(uint256 index) public view returns (uint256 answerBlockNumber, address bettor, bytes1 challenges) {
+        BetInfo memory b = _bets[index];
+        answerBlockNumber = b.answerBlockNumber;
+        bettor = b.bettor;
+        challenges = b.challenges;
+    }
+
+    function pushBet(bytes1 challenges) internal returns (bool) {
+        BetInfo memory b;
+        b.bettor = payable(msg.sender);
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;
+        b.challenges = challenges;
+
+        _bets[_tail] = b;
+        _tail++;
+        return true;
+    }
+
+    function popBet(uint256 index) internal returns (bool) {
+        delete _bets[index];
+        return true;
     }
 }
